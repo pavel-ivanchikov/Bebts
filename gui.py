9a6b4d0bc30a83493f479700f3eb3c37acfb4bfb
pavel <pavel.ivanchikov@gmail.com>
2024-09-09 13:46:29 +0700

    Добавил в этот файл десиарилизацию, и уже вывел названия процессов на кнопках. Так и не понял как менять размеры основного окна...

--- gui.py
+++ gui.py
@@ -1,7 +1,12 @@
-# Python tkinter hello world program
+from ProcessesManager import ProcessesManager
 from tkinter import *
 root = Tk()
-a = Label(root, text="Hello World")
+a = Label(root, text="Debt Counter")
+
+path = r"C:/DebtCounter/first/"
+first_process_name = '1725860502812180'
+
+pm = ProcessesManager(path, first_process_name)
 
 
 def change():
@@ -11,10 +16,19 @@ def change():
         button['text'] = 'Putin'
 
 
+buttons = {}
+for name in pm.main_dict.keys():
+    buttons[name] = Button(root, text=name, command=quit)
+
+
 button = Button(root, text='Hi', command=change)
+button2 = Button(root, text='Close', command=quit)
 
 a.pack()
 button.pack()
+for i in buttons.values():
+    i.pack()
+button2.pack()
 root.mainloop()
 
 
